e9494f2a1c138c6b5f063f7daa81dbf49f93596b
Francesco Saverio De Luca <f.saverio.deluca@gmail.com>
2023-03-21 15:14:49 +0100

    changed something

--- api/User/add.js
+++ api/User/add.js
@@ -1,8 +1,21 @@
 import { connectMongo } from "../../utils/connectMongo";
 import User from "../../models/userModel";
+import { connect } from "mongoose";
 
 export default async function addUser(req, res) {
+  try {
+    console.log('Connect to mongo');
+    await connectMongo();
+    console.log('Connect to mongo');
+    console.log('Adding User');
+    const user = await User.create(req.body);
+    console.log('user added');
+    console.log('user' + user);
+  } catch (error) {
+    console.log(error);
+    res.json({ error: error });
+  }
   const { name, email } = req.body;
   await connectMongo();
-  const user = await User.create(req.body); 
-}
+  const user = await User.create(req.body);
+} 
--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,15 @@
 import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-  name: String,
-  email: {
-    type: string,
-    required: true,
-    unique: true
+  email:{
+  type: String,
+  required: true,
+  unique: true
   },
+  password: {
+    type: String, 
+    required: true
+  }
 });
 
 const User = models.User || model('User', userSchema);
